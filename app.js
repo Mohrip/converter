7a576299e3718768b53bb1d8373e20949943f7fd
Mohammed Alsaif <123654677+Mohrip@users.noreply.github.com>
2024-07-10 12:05:21 +0300

    Updates...

--- app.js
+++ app.js
@@ -5,13 +5,11 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-//include the express-session module
 var session = require('express-session');
-
+//This is a very basic version of the photo convertor... it's not fully implemented
 
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -20,7 +18,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-//activate it as an express.js middleware
 app.use( session({secret: 'YOUR_SECRET'}) )
 
 app.use('/', indexRouter);
